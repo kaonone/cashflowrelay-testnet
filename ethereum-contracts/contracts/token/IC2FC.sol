cb1f7da871013717a7e2a5cf83de363c9947eabd
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-01-28 18:52:41 +0300

    Interface for payments functions

--- ethereum-contracts/contracts/token/IC2FC.sol
+++ ethereum-contracts/contracts/token/IC2FC.sol
@@ -7,10 +7,20 @@ import "../introspection/IERC165.sol";
 
 contract IC2FC is IERC165 {
 
-    event Cashflow(string name, uint256 value, uint256 commit, uint256 interestRate, uint256 duration, uint256 indexed tokenId);
+    event Cashflow(
+        address subscriber, 
+        string name, 
+        uint256 value, 
+        uint256 commit, 
+        uint256 interestRate, 
+        uint256 duration, 
+        uint256 indexed tokenId
+    );
    
+    
     function cashflowFor(uint256 tokenId) public view returns
     (
+     address subscriber,
      uint256 balance, 
      string memory name, 
      uint256 value,
--- ethereum-contracts/contracts/token/IC2FCPayments.sol
+++ ethereum-contracts/contracts/token/IC2FCPayments.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.2;
 
 contract IC2FCPayments {
-
     event ExecuteSubscription(
         address indexed from, //the subscriber
         address indexed to, //the publisher
@@ -17,4 +16,24 @@ contract IC2FCPayments {
         uint256 tokenAmount, //the token amount paid to the publisher
         uint256 datePayment
     );
+    
+    function isSubscriptionActive (uint tokenId) public view returns (bool);
+
+    function cancelSubscription(
+        address to, //the publisher
+        address tokenAddress, //the token address paid to the publisher
+        uint256 tokenAmount
+    ) external
+        returns (bool success);
+
+
+    function executeSubscription(
+        address to, //the publisher
+        address tokenAddress, //the token address paid to the publisher
+        uint256 tokenAmount //the token amount paid to the publisher
+    ) public
+        returns (bool success);
+         
+        
+    function withdrawCashflow(uint256 amount) public returns (bool);
 }

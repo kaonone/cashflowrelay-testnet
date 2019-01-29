3112fb1b41bf08366a8bfcd6bd53e25b79c5bc1c
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-01-29 15:40:06 +0300

    C2FC Implemenatation

--- ethereum-contracts/contracts/token/C2FCFull.sol
+++ ethereum-contracts/contracts/token/C2FCFull.sol
@@ -11,7 +11,55 @@ import "../ownership/Ownable.sol";
 
 contract C2FCFull is ERC721Full, ERC721Mintable, Ownable {
 
+    //Cashflow struct
+    struct Cashflow {
+        string name;
+        uint256 value; 
+        uint256 commit;
+        uint256 interestRate; 
+        uint256 duration;
+    }
+
+    //index => Cashflows store
+    mapping (uint256 =>Cashflow) private _cashflowsIndex;
+
     constructor (string memory name, string memory symbol) public ERC721Full(name, symbol) {
         // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function createCashFlow(
+        string memory name, 
+        uint256 value, 
+        uint256 commit, 
+        uint256 interestRate, 
+        uint256 duration
+        ) 
+        public returns (bool) 
+    {
+        
+    }
+
+    function cashflowFor(uint256 tokenId) public view returns
+    (
+     address subscriber,
+     uint256 balance, 
+     string memory name, 
+     uint256 value,
+     uint256 commit, 
+     uint256 interestRate,
+     uint256 duration, 
+     uint256 firstPayment,
+     uint256 lastPayment
+     ) 
+    {
+
+    }
+
+    function  idsOfCashflowsFor(address _owner) public view returns 
+    (
+        uint256[] memory tokenIds
+    )
+    {
+        
     }
 }

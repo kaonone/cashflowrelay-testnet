0316e928723ec3fe4c22e908df0b490493a615c2
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-01-31 16:12:36 +0300

    Add orders for payments

--- ethereum-contracts/contracts/token/IC2FCPayments.sol
+++ ethereum-contracts/contracts/token/IC2FCPayments.sol
@@ -12,6 +12,24 @@ contract IC2FCPayments is IC2FC {
         uint256 datePayment //the period in seconds between payments
     );
 
+    event ExecuteOrder(
+        uint256 tokenId,
+        address indexed from, //the subscriber
+        address indexed to, //the publisher
+        address tokenAddress, //the token address paid to the publisher
+        uint256 tokenAmount, //the token amount paid to the publisher
+        uint256 datePayment //the period in seconds between payments
+    );
+
+    event CreateOrder(
+        uint256 tokenId,
+        address indexed from, //the subscriber
+        address indexed to, //the publisher
+        address tokenAddress, //the token address paid to the publisher
+        uint256 tokenAmount, //the token amount paid to the publisher
+        uint256 datePayment //the period in seconds between payments
+    );
+
     event CancelPayment(
         uint256 tokenId,
         address indexed from, //the subscriber
@@ -20,7 +38,7 @@ contract IC2FCPayments is IC2FC {
         uint256 tokenAmount, //the token amount paid to the publisher
         uint256 datePayment
     );
-    
+
     function isPaymentsActive (uint tokenId) public view returns (bool);
 
     function cancelPayment(
@@ -31,7 +49,30 @@ contract IC2FCPayments is IC2FC {
     ) external
         returns (bool success);
 
+    function createOrder(        
+        uint256 tokenId,
+        address to, //the publisher
+        address tokenAddress, //the token address paid to the publisher
+        uint256 tokenAmount //the token amount paid to the publisher)
+    )   public
+        returns (bool success);
+    
+    function executeOrder(
+        uint256 orderId //orderId
+    ) public
+        returns (bool success);
 
+    function getByOrderId(
+        uint256 orderId //OrderId
+    ) public
+        returns (uint256 pendingDatePayment, uint256 datePayment, uint256 amount, bool status);
+
+    function getOrdersList(
+        uint256 tokenId //tokenId
+    ) public
+        returns (uint256[] orderIds);
+
+    
     function executePayment(
         uint256 tokenId,
         address to, //the publisher
@@ -40,6 +81,6 @@ contract IC2FCPayments is IC2FC {
     ) public
         returns (bool success);
          
-        
+    
     function withdrawPayments(uint256 tokenId, uint256 amount) public returns (bool);
 }

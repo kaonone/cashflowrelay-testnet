cc2723f1d8d6070a6a973366c0460a5874ca1991
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-02-06 17:38:13 +0300

    Minor fixes

--- ethereum-contracts/contracts/token/IC2FC.sol
+++ ethereum-contracts/contracts/token/IC2FC.sol
@@ -38,6 +38,20 @@ contract IC2FC is IERC165 {
     );
    
     
+     /**
+     * @dev Function for get Cashflow
+     * @param tokenId uint256 ID of the token (base token, for example, ERC721)
+     * @return 
+     * subscriber address, who create and subscribe for payment to cashflow
+     * name string name of Cashflow
+     * value uint256 maximum value for Cashflow
+     * commit uin256 unit for payment in period (default, month)
+     * interestRate uin256 size of interest above value
+     * duration uint256 period of Cashflow
+     * balance uint256 ID of the token (base token, for example, ERC721)
+     * created timestamp date created 
+     */
+
     function cashflowFor(uint256 tokenId) public view returns
     (
         address subscriber,
@@ -51,16 +65,35 @@ contract IC2FC is IERC165 {
         uint256 lastPayment
         );
 
-
+    /**
+     * @dev Function for get balance of Cashflow
+     * @param tokenId uint256 ID of the token (base token, for example, ERC721)
+     * @return uint256 value
+     */
     function balanceOfCashflowFor(uint256 tokenId) public view returns
     (
         uint256 balance
         );
 
+     /**
+     * @dev Function get list of token for user address
+     * @param _owner address of user
+     * @return uint256[] array of IDs
+     */
+
     function  idsOfCashflowsFor(address _owner) public view returns 
     (
         uint256[] memory tokenIds
         );
+
+    /**
+     * @dev @dev Function for create Cashflow
+     * @param name string name of Cashflow
+     * @param value uint256 maximum value for Cashflow
+     * @param commit uin256 unit for payment in period (default, month)
+     * @param interestRate uin256 size of interest above value
+     * @param duration uint256 period of Cashflow
+     */
 
     function createCashFlow(
         string memory name, 

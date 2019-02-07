535f837094202f287cf09f34b085bf59100039d6
Dmitry Hursevich <Varimartas@yandex.ru>
2019-02-08 00:23:35 +0700

    Preloaders to permissions (#44)
    * add preloaders to permissions
    
    * remove warning
    
    * add message to global preloader

--- src/services/i18n/locales/en.ts
+++ src/services/i18n/locales/en.ts
@@ -20,6 +20,7 @@ export default {
     noEthereumConnection: 'No connection to the Etherium network',
     noEthereumAccounts: 'We can\'t find any Ethereum accounts! Please check and make sure Metamask or your browser are pointed at the correct network and your account is unlocked.',
     needUseMetamask: 'This browser has no connection to the Ethereum network. Please use the Chrome/FireFox extension MetaMask, or dedicated Ethereum browsers Mist or Parity.',
+    makeSureUseKovan: 'Please make sure you are using Kovan Test Network when connecting your Metamask',
   },
   components: {
     fund: {
@@ -87,7 +88,7 @@ export default {
         },
       },
       permissions: {
-        title: 'Authorise Cashflow Relay to:',
+        title: 'In the context of this hackaton, please authorise Cashflow Relay to perform these operations:',
         createCashflows: 'Create cashflows',
         sellCashflows: 'Sell cashflows',
         payInstalments: 'Pay instalments from your wallet',

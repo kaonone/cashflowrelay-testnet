826ba01d67c80f4a105d580f1487c43b1ef06dd7
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-02-18 09:45:25 +0300

    Changed casflow name validation

--- src/shared/validators/allowedCharactersForCashFlowName.ts
+++ src/shared/validators/allowedCharactersForCashFlowName.ts
@@ -1,10 +1,8 @@
 import { tKeys, ITranslateKey } from 'services/i18n';
 
 export function allowedCharactersForCashFlowName(value: string): ITranslateKey | undefined {
-  const specificChars = ' .,?!-#$%&*\'"';
-  const validationRegExp = new RegExp(`^(\\w|\\d|[${specificChars}])+$`);
-  const characters = `[a-z], [A-Z], [${specificChars}]`;
+  const validationRegExp = new RegExp(`^(\\w| )+$`);
   return !validationRegExp.test(value)
-    ? { key: tKeys.shared.validation.allowedCharactersForCashFlowName.getKey(), params: { characters } }
+    ? tKeys.shared.validation.allowedCharactersForCashFlowName.getKey()
     : undefined;
 }
